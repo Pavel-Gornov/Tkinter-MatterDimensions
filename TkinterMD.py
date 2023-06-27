7adc7cc77fe4e5a3ebf7d986b4e1231f559e0aa4
Pavel-Gornov <gornovPavel30@yandex.ru>
2023-06-27 14:57:33 +0400

    1.4

--- TkinterMD.py
+++ TkinterMD.py
@@ -1,5 +1,7 @@
 import json
 import math
+import threading
+import time
 import tkinter.filedialog as fd
 import tkinter.messagebox as mb
 from tkinter import *
@@ -14,8 +16,11 @@ class Game:
     def __init__(self, font="Calibri", refresh_speed=33, saving=False):
         self.font = font
         self.refresh_speed = refresh_speed
+        self.window = Tk()
         self.new_game()
         self.speed = 1000 / self.refresh_speed
+        self.decode_BoolVar = lambda x: x.get()
+        self.load_BoolVar = lambda x: BooleanVar(value=x)
         if saving:
             try:
                 with open("save.json", mode="r") as f:
@@ -23,77 +28,89 @@ class Game:
                     self.load(data)
             except Exception as e:
                 print(e)
+        self.alive = True
+        self.start()
         self.initUI()
 
+    def start(self):
+        self.calc_thread = threading.Thread(target=self.calculations)
+        self.calc_thread.start()
+
     def initUI(self):
-        # Вкладки
-        self.window = Tk()
+        # Инициализация Интерфейса
         self.window.title("Измерения Материи")
         self.window.geometry("500x500")
-
+        # Верхнее Меню
         self.menu = Menu(self.window)
         self.file_menu = Menu(self.menu, tearoff=0)
         self.file_menu.add_command(label='Сохранить', command=self.save)
         self.file_menu.add_command(label='Загрузить', command=self.load_save_from_file)
         self.menu.add_cascade(label='Файл', menu=self.file_menu)
         self.window.config(menu=self.menu)
-
-        self.tab_control = ttk.Notebook(self.window)
-        self.m_txt = Label(self.window, text=f"У Вас: {self.Matter} ед. Материи", font=self.font,
-                           background="light blue", height=2)
+        # Показатель материи
+        self.m_txt = Label(self.window, font=self.font, background="light blue", height=2)
         self.m_txt.pack(anchor="n", fill=X)
+        # Вкладки
+        self.tab_control = ttk.Notebook(self.window)
         self.MD_tab = ttk.Frame(self.tab_control)
         self.stat_tab = ttk.Frame(self.tab_control)
+        self.auto_tab = ttk.Frame(self.tab_control)
         self.tab_control.pack(expand=1, fill='both')
         self.tab_control.add(self.MD_tab, text='Измерения Материи')
+        self.tab_control.add(self.auto_tab, text='Автоматика', state="hidden")
         self.tab_control.add(self.stat_tab, text='Статистика')
-        self.d_grid = ttk.Frame(master=self.MD_tab, relief="solid", borderwidth=1, width=10, height=350)
-        self.btn_grid = ttk.Frame(master=self.MD_tab, width=10, height=350)
+        # Вкладка Автоматики
+        self.auto_grid = ttk.Frame(self.auto_tab, relief="solid", borderwidth=1)
+        self.auto_grid.pack(anchor="n")
+        self.auto = [
+            Checkbutton(self.auto_grid, text="Авто-покупка 1-го Измерения Материи", variable=self.auto_state[0],
+                        font=self.font),
+            Checkbutton(self.auto_grid, text="Авто-покупка 2-го Измерения Материи", variable=self.auto_state[1],
+                        font=self.font),
+            Checkbutton(self.auto_grid, text="Авто-покупка 3-го Измерения Материи", variable=self.auto_state[2],
+                        font=self.font),
+            Checkbutton(self.auto_grid, text="Авто-покупка 4-го Измерения Материи", variable=self.auto_state[3],
+                        font=self.font),
+            Checkbutton(self.auto_grid, text="Авто-покупка Сжатия Измерений", variable=self.auto_state[4],
+                        font=self.font)]
+        for i in range(len(self.auto)):
+            self.auto[i].grid(row=i, column=0, sticky="w")
+
         # Статистика
-        self.MD1_count = Label(self.stat_tab, text=f"Куплено 1-х измерений: {self.MD1_bought}")
-        self.MD2_count = Label(self.stat_tab, text=f"Куплено 2-х измерений: {self.MD2_bought}")
-        self.MD3_count = Label(self.stat_tab, text=f"Куплено 3-х измерений: {self.MD3_bought}")
-        self.MD4_count = Label(self.stat_tab, text=f"Куплено 4-х измерений: {self.MD4_bought}")
+        self.Matter_all_stat = Label(self.stat_tab, width=10, height=2, background="light blue1", font=self.font)
+        self.MD1_count = Label(self.stat_tab, font=self.font)
+        self.MD2_count = Label(self.stat_tab, font=self.font)
+        self.MD3_count = Label(self.stat_tab, font=self.font)
+        self.MD4_count = Label(self.stat_tab, font=self.font)
+        self.Matter_all_stat.pack(anchor="n", fill='both', padx=10, pady=5)
         self.MD1_count.pack(anchor="n", fill='both', padx=10, pady=5)
         self.MD2_count.pack(anchor="n", fill='both', padx=10, pady=5)
         self.MD3_count.pack(anchor="n", fill='both', padx=10, pady=5)
         self.MD4_count.pack(anchor="n", fill='both', padx=10, pady=5)
 
         # Измерения Материи
+        self.d_grid = ttk.Frame(master=self.MD_tab, relief="solid", borderwidth=1, width=10, height=350)
+        self.btn_grid = ttk.Frame(master=self.MD_tab, width=10, height=350)
 
-        self.md1_txt = Label(self.d_grid, text=f"1-е Измерение Материи: {self.MD1}\nМножитель: x{self.MD1_mult}",
-                             font=self.font, background="light blue3", anchor="w", padx=20, height=2, width=34,
-                             justify="left")
-        self.md1_btn = Button(self.d_grid, command=self.md1_btn_click,
-                              text=f"Купить 1-е измерение!\nЦена: {int(self.MD1_price)} м.", height=2)
+        self.md1_txt = Label(self.d_grid, font=self.font, background="light blue3", anchor="w", padx=20, height=2,
+                             width=34, justify="left")
+        self.md1_btn = Button(self.d_grid, command=self.md1_btn_click, height=2)
+        self.md2_txt = Label(self.d_grid, font=self.font, background="light blue", anchor="w", padx=20, height=2,
+                             width=34, justify="left")
+        self.md2_btn = Button(self.d_grid, command=self.md2_btn_click, height=2)
 
-        self.md2_txt = Label(self.d_grid, text=f"2-е Измерение Материи: {self.MD1}\nМножитель: x{self.MD2_mult}",
-                             font=self.font, background="light blue", anchor="w", padx=20, height=2, width=34,
-                             justify="left")
-        self.md2_btn = Button(self.d_grid, command=self.md2_btn_click,
-                              text=f"Купить 2-е измерение!\nЦена: {int(self.MD1_price)} м.", height=2)
+        self.md3_txt = Label(self.d_grid, font=self.font, justify="left", background="light blue3", anchor="w", padx=20,
+                             height=2, width=34)
+        self.md3_btn = Button(self.d_grid, command=self.md3_btn_click, height=2)
 
-        self.md3_txt = Label(self.d_grid, text=f"3-е Измерение Материи: {self.MD1}\nМножитель: x{self.MD3_mult}",
-                             font=self.font, justify="left",
-                             background="light blue3", anchor="w", padx=20, height=2, width=34)
-        self.md3_btn = Button(self.d_grid, command=self.md3_btn_click,
-                              text=f"Купить 3-е измерение!\nЦена: {int(self.MD1_price)} м.", height=2)
-
-        self.md4_txt = Label(self.d_grid, text=f"4-е Измерение Материи: {self.MD1}\nМножитель: x{self.MD4_mult}",
-                             font=self.font, justify="left",
-                             background="light blue", anchor="w", padx=20, height=2, width=34)
-        self.md4_btn = Button(self.d_grid, command=self.md4_btn_click,
-                              text=f"Купить 4-е измерение!\nЦена: {int(self.MD1_price)} м.", height=2)
-        self.max_btn = Button(self.btn_grid, command=self.max,
-                              text="Купить всё", height=2, width=20)
-        self.max2_btn = Button(self.btn_grid, command=self.max2,
-                               text="Купить всё 2.0", height=2, width=20)
-        self.crunch_btn = Button(self.btn_grid, command=self.crunch,
-                                 text="Сжатие измерений", height=2, width=20)
+        self.md4_txt = Label(self.d_grid, font=self.font, justify="left", background="light blue", anchor="w", padx=20,
+                             height=2, width=34)
+        self.md4_btn = Button(self.d_grid, command=self.md4_btn_click, height=2)
+        self.max_btn = Button(self.btn_grid, command=self.max, text="Купить всё", height=2, width=20)
+        self.max2_btn = Button(self.btn_grid, command=self.max2, text="Купить всё 2.0", height=2, width=20)
+        self.crunch_btn = Button(self.btn_grid, command=self.crunch, text="Сжатие Измерений", height=2, width=20)
         self.singularity_pb = ttk.Progressbar(self.MD_tab, mode="determinate")
-        self.to_singularity_txt = Label(self.MD_tab, text="Прогресс до сингулярности:", font=self.font,
-                                        background="green3")
-
+        self.to_singularity_txt = Label(self.MD_tab, font=self.font, background="green3")
         # Упаковка
         self.d_grid.pack(anchor="n", padx=10, pady=10)
         self.md1_txt.grid(row=0, column=0, padx=5, pady=5, columnspan=2, sticky="ew")
@@ -104,14 +121,14 @@ class Game:
         self.md3_btn.grid(row=2, column=2, padx=5, pady=5)
         self.md4_txt.grid(row=3, column=0, padx=5, pady=5, columnspan=2)
         self.md4_btn.grid(row=3, column=2, padx=5, pady=5)
-
+        # Кнопки
         self.btn_grid.pack(anchor="n", padx=10, pady=10)
         self.max_btn.grid(row=0, column=0, padx=5, pady=5)
         self.max2_btn.grid(row=1, column=0, padx=5, pady=5)
         self.crunch_btn.grid(row=0, column=3, padx=[160, 5])
         self.to_singularity_txt.pack(anchor="nw", fill='x', padx=10, pady=10)
         self.singularity_pb.pack(anchor="nw", fill='x', padx=10)
-
+        # Запуск цикла
         self.window.after(0, self.main_loop)
         self.window.mainloop()
 
@@ -186,9 +203,27 @@ class Game:
             self.MD4_price = 1_000_000
 
     def main_loop(self):
-        self.calculations()
         self.UI_refresh()
+        self.unlocks()
+        self.auto_buyers()
         self.window.after(self.refresh_speed, self.main_loop)
+
+    def auto_buyers(self):
+        data = list(map(self.decode_BoolVar, self.auto_state))
+        if data[0]:
+            self.md1_btn_click()
+        if data[1]:
+            self.md2_btn_click()
+        if data[2]:
+            self.md3_btn_click()
+        if data[3]:
+            self.md4_btn_click()
+        if data[4]:
+            self.crunch()
+
+    def unlocks(self):
+        if self.Matter_all >= float("1e70") and self.tab_control.tab(1)["state"] != "normal":
+            self.tab_control.tab(1, state="normal")
 
     def UI_refresh(self):
         current_tab = self.tab_control.tab(self.tab_control.select(), "text")
@@ -217,28 +252,34 @@ class Game:
             self.singularity_pb['value'] = temp
             self.to_singularity_txt["text"] = f"Прогресс до сингулярности: {round(temp, 1)}%"
         elif current_tab == 'Статистика':
+            self.Matter_all_stat["text"] = f"Всего было получено: {num_notation(round(self.Matter_all, 1))} Материи."
             self.MD1_count["text"] = f"Куплено 1-х измерений: {self.MD1_bought}"
             self.MD2_count["text"] = f"Куплено 2-х измерений: {self.MD2_bought}"
             self.MD3_count["text"] = f"Куплено 3-х измерений: {self.MD3_bought}"
             self.MD4_count["text"] = f"Куплено 4-х измерений: {self.MD4_bought}"
 
     def calculations(self):
-        self.Matter += (self.MD1 * self.MD1_mult * (2 ** self.MCrunch)) / self.speed
-        self.MD1 += (self.MD2 * self.MD2_mult * (2 ** self.MCrunch)) / self.speed
-        self.MD2 += (self.MD3 * self.MD3_mult * (2 ** self.MCrunch)) / self.speed
-        self.MD3 += (self.MD4 * self.MD4_mult * (2 ** self.MCrunch)) / self.speed
-        self.MD1_mult = ((self.MD1_bought // 10) + 1) * (2 ** self.MCrunch)
-        self.MD2_mult = ((self.MD2_bought // 10) + 1) * (2 ** self.MCrunch)
-        self.MD3_mult = ((self.MD3_bought // 10) + 1) * (2 ** self.MCrunch)
-        self.MD4_mult = ((self.MD4_bought // 10) + 1) * (2 ** self.MCrunch)
+        while self.alive:
+            time.sleep(self.refresh_speed / 1000)
+            temp = (self.MD1 * self.MD1_mult * (2 ** self.MCrunch)) / self.speed
+            self.Matter += temp
+            self.Matter_all += temp
+            self.MD1 += (self.MD2 * self.MD2_mult * (2 ** self.MCrunch)) / self.speed
+            self.MD2 += (self.MD3 * self.MD3_mult * (2 ** self.MCrunch)) / self.speed
+            self.MD3 += (self.MD4 * self.MD4_mult * (2 ** self.MCrunch)) / self.speed
+            self.MD1_mult = ((self.MD1_bought // 10) + 1) * (2 ** self.MCrunch)
+            self.MD2_mult = ((self.MD2_bought // 10) + 1) * (2 ** self.MCrunch)
+            self.MD3_mult = ((self.MD3_bought // 10) + 1) * (2 ** self.MCrunch)
+            self.MD4_mult = ((self.MD4_bought // 10) + 1) * (2 ** self.MCrunch)
 
     def save(self):
-        save = {"Matter": self.Matter, "MD1": self.MD1, "MD2": self.MD2, "MD3": self.MD3, "MD4": self.MD4,
-                "MD1_bought": self.MD1_bought, "MD2_bought": self.MD2_bought, "MD3_bought": self.MD3_bought,
-                "MD4_bought": self.MD4_bought, "MD1_mult": self.MD1_mult, "MD2_mult": self.MD2_mult,
-                "MD3_mult": self.MD3_mult, "MD4_mult": self.MD4_mult, "MD1_price": self.MD1_price,
-                "MD2_price": self.MD2_price, "MD3_price": self.MD3_price, "MD4_price": self.MD4_price,
-                "MCrunch": self.MCrunch, "MCrunch_cost": self.MCrunch_cost}
+        save = {"Matter": self.Matter, "Matter_all": self.Matter_all, "MD1": self.MD1, "MD2": self.MD2, "MD3": self.MD3,
+                "MD4": self.MD4, "MD1_bought": self.MD1_bought, "MD2_bought": self.MD2_bought,
+                "MD3_bought": self.MD3_bought, "MD4_bought": self.MD4_bought, "MD1_mult": self.MD1_mult,
+                "MD2_mult": self.MD2_mult, "MD3_mult": self.MD3_mult, "MD4_mult": self.MD4_mult,
+                "MD1_price": self.MD1_price, "MD2_price": self.MD2_price, "MD3_price": self.MD3_price,
+                "MD4_price": self.MD4_price, "MCrunch": self.MCrunch, "MCrunch_cost": self.MCrunch_cost,
+                "auto_state": list(map(self.decode_BoolVar, self.auto_state))}
         with open("save.json", mode="w") as f:
             f.write(json.dumps(save, indent=2))
 
@@ -258,6 +299,7 @@ class Game:
 
     def load(self, data):
         self.Matter = data["Matter"]
+        self.Matter_all = data["Matter_all"]
         self.MD1 = data["MD1"]
         self.MD2 = data["MD2"]
         self.MD3 = data["MD3"]
@@ -276,9 +318,11 @@ class Game:
         self.MD4_price = data["MD4_price"]
         self.MCrunch = data["MCrunch"]
         self.MCrunch_cost = data["MCrunch_cost"]
+        self.auto_state = list(map(self.load_BoolVar, data["auto_state"]))
 
     def new_game(self):
         self.Matter = 10
+        self.Matter_all = 10
         self.MD1 = 0
         self.MD2 = 0
         self.MD3 = 0
@@ -297,6 +341,7 @@ class Game:
         self.MD4_price = 1_000_000
         self.MCrunch = 0
         self.MCrunch_cost = 10
+        self.auto_state = [BooleanVar(), BooleanVar(), BooleanVar(), BooleanVar(), BooleanVar()]
 
 
 def format_e(n):
@@ -323,4 +368,5 @@ if __name__ == "__main__":
     try:
         g = Game(saving=True)
     finally:
+        g.alive = False
         g.save()
